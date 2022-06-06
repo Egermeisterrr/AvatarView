f29cb8d2d80eaaeb41dd78f953ed1aad29804ebe
Egor <egor.fokin.3005@mail.ru>
2022-06-06 21:29:04 +0300

    ready made avatar view

--- app/src/main/java/com/example/myview/ui/AvatarViewMask.kt
+++ app/src/main/java/com/example/myview/ui/AvatarViewMask.kt
@@ -1,35 +1,47 @@
 package com.example.myview.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.widget.ImageView
+import androidx.annotation.ColorInt
+import androidx.annotation.Px
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.graphics.toRectF
 import com.example.myview.R
 import com.example.myview.extentions.dpToPx
 
 
+@SuppressLint("Recycle")
 class AvatarViewMask @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-) : AppCompatImageView(context, attrs, defStyleAttr) {
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
 
     companion object {
-        private const val DEFAULT_SIZE = 40
-        private const val DEFAULT_BORDER_COLOR = Color.WHITE
         private const val DEFAULT_BORDER_WIDTH = 2
+        private const val DEFAULT_BORDER_COLOR = Color.WHITE
+        private const val DEFAULT_SIZE = 40
     }
 
+    @Px
     var borderWidth: Float = context.dpToPx(DEFAULT_BORDER_WIDTH)
+
+    @ColorInt
     private var borderColor: Int = Color.WHITE
     private var initials: String = "??"
 
     private val maskPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val viewRect = Rect()
+
     private lateinit var resultBm: Bitmap
     private lateinit var maskBm: Bitmap
     private lateinit var srcBm: Bitmap
+
 
     init {
         if (attrs != null) {
@@ -40,7 +52,10 @@ class AvatarViewMask @JvmOverloads constructor(
             )
             borderColor =
                 ta.getColor(R.styleable.AvatarViewMask_aiv_borderColor, DEFAULT_BORDER_COLOR)
+
             initials = ta.getString(R.styleable.AvatarViewMask_aiv_initials) ?: "??"
+
+            ta.recycle()
         }
 
         scaleType = ScaleType.CENTER_CROP
@@ -48,7 +63,7 @@ class AvatarViewMask @JvmOverloads constructor(
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        //super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val initSize = resolveDefaultSize(widthMeasureSpec)
         setMeasuredDimension(initSize, initSize)
     }
@@ -71,15 +86,20 @@ class AvatarViewMask @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        canvas?.drawBitmap(maskBm, viewRect, viewRect, null)
+        //super.onDraw(canvas)
+        canvas?.drawBitmap(resultBm, viewRect, viewRect, null)
+
+        val half = (borderWidth / 2).toInt()
+        viewRect.inset(half, half)
+
+        canvas?.drawOval(viewRect.toRectF(), borderPaint)
     }
 
     private fun resolveDefaultSize(spec: Int): Int {
         return when (MeasureSpec.getMode(spec)) {
             MeasureSpec.UNSPECIFIED -> context.dpToPx(DEFAULT_SIZE).toInt()
-            MeasureSpec.EXACTLY -> MeasureSpec.getSize(spec)
             MeasureSpec.AT_MOST -> MeasureSpec.getSize(spec)
+            MeasureSpec.EXACTLY -> MeasureSpec.getSize(spec)
             else -> MeasureSpec.getSize(spec)
         }
     }
@@ -89,14 +109,27 @@ class AvatarViewMask @JvmOverloads constructor(
             color = Color.RED
             style = Paint.Style.FILL
         }
+
+        with(borderPaint) {
+            style = Paint.Style.STROKE
+            strokeWidth = borderWidth
+            color = borderColor
+        }
     }
 
     private fun prepareBitmaps(w: Int, h: Int) {
         maskBm = Bitmap.createBitmap(w, h, Bitmap.Config.ALPHA_8)
+        resultBm = maskBm.copy(Bitmap.Config.ARGB_8888, true)
 
         val maskCanvas = Canvas(maskBm)
         maskCanvas.drawOval(viewRect.toRectF(), maskPaint)
 
-        resultBm = maskBm.copy(Bitmap.Config.ARGB_8888, true)
+        maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        srcBm = drawable.toBitmap(w, h, Bitmap.Config.ARGB_8888)
+
+        val resultCanvas = Canvas(resultBm)
+
+        resultCanvas.drawBitmap(maskBm, viewRect, viewRect, null)
+        resultCanvas.drawBitmap(srcBm, viewRect, viewRect, maskPaint)
     }
 }
